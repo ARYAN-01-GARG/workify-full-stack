5ea6cc31eda5084029103de7c0e4a5f9cfde7acf
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-06-24 23:58:27 +0530

    Fix : fixed default value issue of limiter

--- server/src/controllers/limiter.ts
+++ server/src/controllers/limiter.ts
@@ -2,8 +2,8 @@ import rateLimit , { RateLimitRequestHandler } from "express-rate-limit";
 
 export const limiter  = ( maxAge : number , maxCount : number ) : RateLimitRequestHandler => {
     return rateLimit({
-    windowMs: maxAge,
-    limit : maxCount,
+    windowMs: maxAge || 100,
+    limit : maxCount || 10 * 60 * 1000,
     standardHeaders: 'draft-8',
     legacyHeaders: true,
     message: 'Too many requests from this IP, please try again after 10 minutes',
