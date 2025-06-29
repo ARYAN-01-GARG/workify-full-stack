93ad747ee550804d44e97006b62bdcfe5a042d12
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-06-29 14:38:31 +0530

    Add : Added Search Feature and some toolkit

--- client/src/pages/posts/PostPage.tsx
+++ client/src/pages/posts/PostPage.tsx
@@ -1,13 +1,31 @@
 import PostCard from "@/components/posts/PostCard";
 import SearchPosts from "@/components/posts/SearchPosts";
 import { selectPosts } from "@/store/features/postsSlice"
+// import { useState } from "react";
 import { useSelector } from "react-redux"
 
 export default function PostPage() {
   const jobs = useSelector(selectPosts);
+
+  // const [filterOptions, setFilterOptions] = useState({
+  //   search: "",
+  //   offerMin: 0,
+  //   offerMax: 100000,
+  //   location: "",
+  //   remote: false,
+  //   skills: [],
+  //   experience: 0,
+  // });
+
+  // const handleFilterChange = (newFilters) => {
+  //   setFilterOptions((prev) => ({ ...prev, ...newFilters }));
+  // };
+
+
   return (
     <main className="w-full bg-[#E6ECF8] min-h-screen pt-12">
       <SearchPosts />
+      <span className="text-lg px-14 font-semibold">{`${jobs.length} Results `}</span>
       <div className="flex px-4 lg:px-10 py-10 gap-x-10 items-start">
         <div className="w-full flex-1 flex flex-col items-center justify-start gap-8 ">
           {jobs.length > 0 ? (
